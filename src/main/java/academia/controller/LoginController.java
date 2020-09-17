ae6952b76a21c307d3ded9ed23cf34afaf326efb
javaee <javaee@javaee-VirtualBox>
2020-09-17 17:10:34 +0200

    Guarda los mensajes del usuario en la sesión.

--- src/main/java/academia/controller/LoginController.java
+++ src/main/java/academia/controller/LoginController.java
@@ -7,8 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
+
+import academia.modelo.Mensaje;
 import academia.modelo.dao.impl.UsuarioDAOImpl;
 import academia.modelo.pojo.Usuario;
 
@@ -19,7 +21,7 @@ import academia.modelo.pojo.Usuario;
 public class LoginController extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	
+	private static final Logger LOG = Logger.getLogger(LoginController.class);
 
 	
 	/**
@@ -34,12 +36,11 @@ public class LoginController extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("static-access")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		UsuarioDAOImpl dao = UsuarioDAOImpl.getInstance();
-		Usuario usuario = new Usuario();
 		String url = "";
-		String mensaje = "";
 		
 		
 		// Recogida de parámetros del formulario.
@@ -49,31 +50,32 @@ public class LoginController extends HttpServlet {
 		
 		try {
 			
-			usuario = dao.buscar(nombre, contrasena); // Busca el usuario en base a los parámetros.
+			Usuario usuario = dao.buscar(nombre, contrasena); // Busca el usuario en base a los parámetros.
 			
 			if (usuario != null) { // Si encuentra un usuario con las credenciales correctas.
 				
-				if (usuario.getRol() == 1) {
-					url = "views/privado/alumno.jsp";
+				request.getSession().setAttribute("usuario_sesion", usuario); // Guarda el usuario en la sesión.
+				
+				if (usuario.getRol() == usuario.ROL_ALUMNO) {
+					url = "views/privado/alumno"; // CursoAlumnoController
 					
 				} else {
-					url = "views/privado/profesor.jsp";
+					url = "views/privado/profesor"; // CursoProfesorController
 				}
 				
-				mensaje = "Has iniciado sesión correctamente.";
+				// En los sendRedirect se pierden los atributos. Hay que guardarlos en la sesión.
+				request.getSession().setAttribute("mensaje", new Mensaje("success","Has iniciado sesión correctamente."));
+				response.sendRedirect(url);
 				
 			} else { // Si no coinciden las credenciales o una o varias están vacías.
 				
-				url = "views/login.jsp";
-				mensaje = "El nombre de usuario y/o la contraseña no son válidos. Introduce los dos correctamente.";
+				request.setAttribute("mensaje", new Mensaje("warning","El nombre de usuario y/o la contraseña no son válidos. Introduce los dos correctamente."));
+				request.getRequestDispatcher("views/login.jsp").forward(request, response);
 			}
 			
 		} catch (Exception e) {
-			e.printStackTrace();
+			LOG.error(e);
 
-		} finally {
-			request.setAttribute("mensaje", mensaje);
-			request.getRequestDispatcher(url).forward(request, response); // Redirige a la jsp correspondiente.
 		}
 		
 	} // doPost
